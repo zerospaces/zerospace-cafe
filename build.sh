89bcbf98dc6df33a6d774d914c0e5450553bf18d
silvaire-qwq <silvaire_qwq@outlook.com>
2025-05-10 15:29:25 +0800

    Summer Cleaning

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -d "./src/diary" ]; then
-  rm -rf "./src/diary"
-fi
-
-git clone https://github.com/zerospaces/cafe-diaries.git ./src/diary --depth 1
-rm -rf "./src/diary/.git"
-pnpm docs:build
